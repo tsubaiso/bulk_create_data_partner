cd055e4d335db47cadb5a697e87a58077c569890
k-sano <sano@tsubaiso.jp>
2018-10-03 16:57:52 +0900

    スクリプト完成

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# bulk_create_data_partner
+# Bulk Create DataPartner
+ツバイソSDK gem を利用した DataPartner 一括付与スクリプトです。([tsubaiso-sdk](https://github.com/tsubaiso/tsubaiso-sdk-ruby))
+DataPartner については[API ドキュメント#外部連携機能](https://github.com/tsubaiso/tsubaiso-api-doumentation) を参照してください。
+
+
+## 初期設定
+ツバイソSDK gem のインストールが必要です。
+下記の方法でインストールしてください。
+
+    $ gem install tsubaiso-sdk
+
+config.yml に base_url と access_token を記述します。
+
+## 使い方
+CSVファイルに "resource","object_id","data_partner - id_code","data_partner - link_url" を記述して、コマンドの引数にファイル名を指定します。
+
+    $ ruby bulk_create_data_partner.rb sample.csv
+
+## CSVファイル形式
+Header code | Necessity | Description
+--- | --- | --- | ---
+`resource` | *required* | DataPartner を付与するオブジェクトの種類。[API ドキュメント](https://github.com/tsubaiso/tsubaiso-api-doumentation) のリソース名と同じです。
+`id` | *required* | DataPartner を付与するオブジェクトのID.
+`id_code` | *option* | 作成する DataPartner の id_code パラメータ。
+`link_url` | *option* | 作成する DataPartner の link_url パラメータ。
+
+
--- bulk_create_data_partner.rb
+++ bulk_create_data_partner.rb
@@ -1,36 +1,33 @@
 # coding: utf-8
 require 'csv'
-#require 'tsubaiso_sdk'
-require '../../tsubaiso-sdk-ruby/lib/tsubaiso_api'
+require 'yaml'
+require 'tsubaiso_api'
 
 USAGE = 'usage: ruby bulk_create_data_partner.rb [csv_file]'
 
 return puts USAGE if ARGV.size != 1
 
-api = TsubaisoAPI.new(base_url: 'http://burikama.tech/tsubaiso.chris_review/eap/', access_token: '6i45jovi7xwvheoa5bzxj25ni-1ay4oo7fxyykmwvwb5ddxpbt0')
+conf = YAML.load_file('./config.yml')
+api = TsubaisoAPI.new(base_url: conf["base_url"], access_token: conf["access_token"])
 
-success_codes, errors = [], []
-
-CSV.foreach(ARGV[0], headers: true, skip_blanks: true) do |line|
-  sleep 0.5
-  # TODO: 多分、IDじゃないと汎用的じゃない
-  resp = api.show(line['resource'], code: line['code'])
+def update(api, line)
+  resp = api.show(line["resource"], id: line["id"])
   resp[:status] == '200' or raise
 
-  object = resp[:json]
-
-  resp = api.update('customer_masters', id: object["id"], code: object["code"], data_partner: { id_code: line["id_code"], link_url: line["link_url"] })
+  code = resp[:json]["code"]
+  resp = api.update(line["resource"], id: line["id"], code: code, data_partner: { id_code: line["id_code"], link_url: line["link_url"] })
   resp[:status] == '200' or raise
 
-  success_codes << line["code"]
-
+  { status: :ok, id: line["id"] }
 rescue
-  errors << "#{line["code"]}: #{resp.dig(:json, "error")}"
+  { status: :error, id: line["id"], message: resp.dig(:json, "error") }
 end
 
-puts "successes: #{success_codes}"
-puts <<EOS
-errors:
-#{errors.join("\n")}
-EOS
+results = CSV.read(ARGV[0], headers: true, skip_blanks: true)
+             .map { |line| sleep(0.5); update(api, line) }
+             .group_by { |result| result[:status] }
 
+errors = results[:error].group_by { |result| result[:message] }
+
+puts "successes: #{results[:ok]&.map { |result| result[:id] }&.join(',')}"
+errors.each { |message, errors| puts "errors: #{message} ... #{errors.map { |result| result[:id] }&.join(',')}" }
